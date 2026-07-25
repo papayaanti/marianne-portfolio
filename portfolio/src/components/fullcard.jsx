da2bb25ee16a790c0d77affff4a396c49f838991
marianne <eliza.marianne.aguhar@gmail.com>
2026-07-25 04:18:24 -0400

    final updates

--- portfolio/src/components/fullcard.jsx
+++ portfolio/src/components/fullcard.jsx
@@ -8,10 +8,10 @@ function FullCard() {
         <img src='profile.webp' alt="pixel art of marianne"/>
 
         <div className='description'>
-            <p className='small-text'>01. about me</p>
+            <p className='small-text'>01. about me/how i work</p>
             <h1>marianne a.</h1>
             <p className='description'>is a <strong>computer science student & communications</strong> student at the university of ottawa.</p>
-            <p className='description'>she uses her creativity and knowledge of user-focused principles to create approachable and efficient designs.</p>
+            <p className='description'>how she works: she uses user-focused principles to create prototypes in figma or with react.js. when designing, she likes to balance accessibility with visual appeal.</p>
         </div>
 
     </div>
